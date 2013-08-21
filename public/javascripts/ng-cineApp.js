eb74ac67d30398f876122750598f555ffbe2ce4a
Sebastien Foubert <sebastien.foubert@gmail.com>
2013-08-21 11:44:49 +0200

    Use angularJS to display modal box content on sample index page

--- public/javascripts/ng-cineApp.js
+++ public/javascripts/ng-cineApp.js
@@ -1,6 +1,14 @@
 var cineApp = angular.module('cineApp', ['infinite-scroll']);
 
-cineApp.controller('CinemaController', function($scope) {
+// allow CORS request control
+cineApp.config(function($httpProvider){
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+
+cineApp.controller('CinemaController', function($scope, $http) {
+  $scope.wait = "/images/wait.gif";
   $scope.movies = [];
 
   $scope.loadMore = function() {
@@ -9,6 +17,17 @@ cineApp.controller('CinemaController', function($scope) {
       $scope.movies.push(movie);
     }
   };
+
+  $scope.showMyModal = function() {
+    console.log("show modal box");
+    $scope.wait = "/images/wait.gif";
+    $http.get('http://mymovieapi.com/?title=Jack Reacher&type=json&plot=simple&episode=1&limit=1&yg=0&mt=none&lang=en-US&offset=&aka=simple&release=simple&business=0&tech=0').success(function(data) {
+        console.log(data);
+        console.log('display cover : ' + data[0].poster.cover);
+        $scope.wait = data[0].poster.cover;
+    });
+  };
+
 });
 
 function Movie(title, viewdate, comment) {
